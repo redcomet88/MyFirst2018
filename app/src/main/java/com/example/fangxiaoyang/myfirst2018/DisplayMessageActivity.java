a536fe643802103ef7fea0d348fb6eec20d42754
redcomet88 <626206333@qq.com>
2019-01-02 08:54:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fangxiaoyang/myfirst2018/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.example.fangxiaoyang.myfirst2018;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+
+    }
+}
